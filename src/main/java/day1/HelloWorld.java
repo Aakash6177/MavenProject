d6e5b200dfb6ceea42ad5fda1d81383c3532f935
Aakash6177 <49572566+Aakash6177@users.noreply.github.com>
2023-04-21 13:35:02 -0700

    Added the another module for factorial.

--- src/main/java/day1/HelloWorld.java
+++ src/main/java/day1/HelloWorld.java
@@ -3,7 +3,7 @@ package day1;
 public class HelloWorld {
 
     public static void main(String[] args){
-        String username = "User Name";
+        String username = "User";
         if(args.length>0){
             username = args[0];
         }
---
+++ src/main/java/practice_1/bankAccount.java
@@ -0,0 +1,16 @@
+package practice_1;
+
+public class bankAccount {
+
+
+    public static void addAmount(double amt){
+
+    }
+
+
+    public static void main(String[] args){
+
+    }
+
+
+}
