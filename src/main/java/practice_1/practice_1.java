6c85f64f4439234055fc79081d54b1037f06a246
Aakash6177 <49572566+Aakash6177@users.noreply.github.com>
2023-04-21 13:35:26 -0700

    Dev local (#2)
    * added extra logic to Helloworld.java and added one more module MultipleTable.java in day1 (#1)
    
    * MAde changes
    
    * MAde changes
    
    * MAde changes
    
    ---------
    
    Co-authored-by: Ananshu <ananshub15@gmail.com>

--- src/main/java/practice_1/practice_1.java
+++ src/main/java/practice_1/practice_1.java
@@ -2,6 +2,8 @@ package practice_1;
 
 public class practice_1 {
     public static void main(String[] args){
+
         System.out.println("Aeyo I made this");
+        System.out.println("Made changes");
     }
 }
