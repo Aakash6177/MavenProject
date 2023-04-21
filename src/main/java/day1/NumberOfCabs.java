c252f10999d244a56a2cd58c3b3671d29aac7f40
Aakash6177 <49572566+Aakash6177@users.noreply.github.com>
2023-04-21 10:30:11 -0700

    Added the another module for factorial.

---
+++ src/main/java/day1/NumberOfCabs.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class NumberOfCabs {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/practice_1/MoviesApplication.java
@@ -0,0 +1,8 @@
+package practice_1;
+
+public class MoviesApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
