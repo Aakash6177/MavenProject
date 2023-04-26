27c16958dca3d6263ef912e931c135b21a821bd1
Aakash6177 <49572566+Aakash6177@users.noreply.github.com>
2023-04-25 23:41:18 -0700

    added palindrome

--- src/main/java/practice_3/palinDrome.java
+++ src/main/java/practice_3/palinDrome.java
@@ -2,4 +2,18 @@ package practice_3;
 
 public class palinDrome {
 
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,2,3,4,5,4,3,2,1,0};
+        boolean isPlaindrome = false;
+        for(int i=0, j=arr.length-1; i<=j; i++, j--){
+            if(arr[i] == arr[j]){
+                isPlaindrome = true;
+                break;
+            }
+        }
+
+        System.out.println("The array is palindrome: "+isPlaindrome);
+
+    }
 }
